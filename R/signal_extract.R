47ede6c1d295c0ff75a73a0c660813892de4c301
deboonstra <daniel-boonstra@uiowa.edu>
2024-02-08 09:43:05 -0600

    Forcing returned objects to be ts and plotting confidence bands

--- R/signal_extract.R
+++ R/signal_extract.R
@@ -14,24 +14,40 @@ signal_extract <- function(data, object) {
   }
 
   # Pulling latent processes ####
-
+  ## Observed
+  obs <- stats::ts(
+    data = c(data),
+    start = stats::tsp(data)[1],
+    freq = stats::tsp(data)[3]
+  )
   ## Trend
   tsm <- stats::ts(
     object$ks$Xs[1, , ], start = stats::tsp(data)[1], freq = stats::tsp(data)[3]
   )
+  ### Standard error
+  tsm_se <- sqrt(object$ks$Ps[1, 1, ])
   ## Seasonal
   ssm <- stats::ts(
     object$ks$Xs[2, , ], start = stats::tsp(data)[1], freq = stats::tsp(data)[3]
   )
+  ### Standard error
+  ssm_se <- sqrt(object$ks$Ps[2, 2, ])
   ## Anomaly and error
   asm <- stats::ts(
-    data - tsm - ssm, start = stats::tsp(data)[1], freq = stats::tsp(data)[3]
+    c(data - tsm - ssm), start = stats::tsp(data)[1], freq = stats::tsp(data)[3]
   )
 
   # Returning object ####
   return(
     structure(
-      .Data = list(obs = data, tsm = tsm, ssm = ssm, asm = asm),
+      .Data = list(
+        obs = obs,
+        tsm = tsm,
+        tsm_se = tsm_se,
+        ssm = ssm,
+        ssm_se = ssm_se,
+        asm = asm
+      ),
       class = "ss.extract"
     )
   )
@@ -39,7 +55,8 @@ signal_extract <- function(data, object) {
 
 ## Creating a plotting function ####
 signal_extract_plot <- function(
-  data, ylim = c(-3, 3), ylab = "Incidence per 100,000", xlab = "Time",
+  data, confidence_band = FALSE, alpha = 0.05,
+  ylim = c(-3, 3), ylab = "Incidence", xlab = "Time",
   main = "", oma = c(2, 2, 0, 0)
 ) {
 
@@ -50,6 +67,7 @@ signal_extract_plot <- function(
 
   # Plotting ####
   graphics::par(mfrow = c(4, 1), oma = oma)
+  crit_val <- stats::qnorm(p = 1 - (alpha / 2))
   ## Observations
   astsa::tsplot(
     x = data$obs, main = "Observed series", ylab = "", xlab = "", ylim = ylim
@@ -58,10 +76,34 @@ signal_extract_plot <- function(
   astsa::tsplot(
     x = data$tsm, main = "Trend component", ylab = "", xlab = "", ylim = ylim
   )
+  if (confidence_band) {
+    xx <- c(stats::time(data$tsm), rev(stats::time(data$tsm)))
+    yy <- c(
+      data$tsm - (crit_val * data$tsm_se),
+      rev(data$tsm + (crit_val * data$tsm_se))
+    )
+    graphics::polygon(
+      x = xx, y = yy,
+      border = NA,
+      col = grDevices::gray(level = 0.5, alpha = 0.3)
+    )
+  }
   ## Seasonal
   astsa::tsplot(
     x = data$ssm, main = "Seasonal component", ylab = "", xlab = "", ylim = ylim
   )
+  if (confidence_band) {
+    xx <- c(stats::time(data$ssm), rev(stats::time(data$ssm)))
+    yy <- c(
+      data$ssm - (crit_val * data$ssm_se),
+      rev(data$ssm + (crit_val * data$ssm_se))
+    )
+    graphics::polygon(
+      x = xx, y = yy,
+      border = NA,
+      col = grDevices::gray(level = 0.5, alpha = 0.3)
+    )
+  }
   ## Anomaly and error
   astsa::tsplot(
     x = data$asm, main = "Local component", ylab = "", xlab = ""
